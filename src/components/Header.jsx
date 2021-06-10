28ff57cbec3eb7dd7579c1b4f208c1f1a96be1b7
Katrina Cloyd <katrina.cloyd@yahoo.com>
2021-06-10 14:21:34 -0700

    Feature/header socket (#7)
    * change cursor color to orange
    
    * create header with click handling to change text
    
    * add socket emit and use effect for incoming socket info for header change
    
    * add periods to header text
    
    Co-authored-by: Katrina Cloyd <‘Katrina.cloyd@yahoo.com’>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,35 @@
-import React, { useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import HeaderNav from './HeaderNav';
 import style from './Header.css';
+import { SocketContext } from '../context/SocketProvider';
+
 
 
 const Header = () => {
 
+  const socket = useContext(SocketContext);
+
   const [headerClicks, setHeaderClicks] = useState(0);
 
+  //NEED TO ADD CLICK LISTENER and ROOM EMIT TO BACKEND
   const upHeaderClickCount = () => {
+    socket.emit('headerTextClick', headerClicks + 1);
     if (headerClicks === 3) setHeaderClicks(0);
     else setHeaderClicks(prev => prev + 1);
-    console.log(headerClicks);
   };
 
-  //NEED TO TIE THIS TO THE SOCKET
+  useEffect(() => {
+    socket.on('socketHeaderTextClick',
+      (newCount) => { setHeaderClicks(newCount); }
+    );
+  }, [socket]);
 
-  let headerText = 'GIM';
+  let headerText = 'G.I.M.';
   if (headerClicks === 1) {
-    headerText = 'Ghost  IM';
+    headerText = 'Ghost  I.M.';
   } else if (headerClicks === 2) {
-    headerText = 'Ghost  In  The  M';
+    headerText = 'Ghost  In  The  M.';
+
   } else if (headerClicks >= 3) {
     headerText = 'Ghost  In  The  Machine';
   }
