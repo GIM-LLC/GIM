bfdb9311f19fa59872b42f2c9bc4629793f40dfe
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-06-10 13:14:55 -0700

    add socket emit and use effect for incoming socket info for header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,26 @@
-import React, { useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import HeaderNav from './HeaderNav';
 import style from './Header.css';
+import { SocketContext } from '../context/SocketProvider';
 
 
 const Header = () => {
+  const socket = useContext(SocketContext);
 
   const [headerClicks, setHeaderClicks] = useState(0);
 
+  //NEED TO ADD CLICK LISTENER and ROOM EMIT TO BACKEND
   const upHeaderClickCount = () => {
+    socket.emit('headerTextClick', headerClicks + 1);
     if (headerClicks === 3) setHeaderClicks(0);
     else setHeaderClicks(prev => prev + 1);
-    console.log(headerClicks);
   };
 
-  //NEED TO TIE THIS TO THE SOCKET
+  useEffect(() => {
+    socket.on('socketHeaderTextClick',
+      (newCount) => { setHeaderClicks(newCount); }
+    );
+  }, [socket]);
 
   let headerText = 'GIM';
   if (headerClicks === 1) {
