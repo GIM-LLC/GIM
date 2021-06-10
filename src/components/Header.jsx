69230812bfeabb0e6519654b23ddee88be44f9ab
Katrina Cloyd <katrina.cloyd@yahoo.com>
2021-06-10 14:17:29 -0700

    Header (#5)
    * change cursor color to orange
    
    * create header with click handling to change text
    
    Co-authored-by: Katrina Cloyd <‘Katrina.cloyd@yahoo.com’>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderNav from './HeaderNav';
 import style from './Header.css';
 
+
 const Header = () => {
+
+  const [headerClicks, setHeaderClicks] = useState(0);
+
+  const upHeaderClickCount = () => {
+    if (headerClicks === 3) setHeaderClicks(0);
+    else setHeaderClicks(prev => prev + 1);
+    console.log(headerClicks);
+  };
+
+  //NEED TO TIE THIS TO THE SOCKET
+
+  let headerText = 'GIM';
+  if (headerClicks === 1) {
+    headerText = 'Ghost  IM';
+  } else if (headerClicks === 2) {
+    headerText = 'Ghost  In  The  M';
+  } else if (headerClicks >= 3) {
+    headerText = 'Ghost  In  The  Machine';
+  }
+
   return (
     <header>
       <span className={style.masthead}>
-        <img src="http://placekitten.com/100/100" alt="logo and/or kitten" />
-        <h1>General Industrial Manufacturing</h1>
+        <h1 onClick={upHeaderClickCount}>
+          {headerText}
+        </h1>
+        <h3>-LLC.-</h3>
       </span>
       <HeaderNav />
     </header>
