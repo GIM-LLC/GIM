3ee0dc38eb98fe271008f56b9672e1fcc8bc82d1
Julianne Vela <hello@juliannevela.dev>
2021-06-10 15:06:59 -0700

    Feature/company profile (#10)
    * Start basic skeleton of site
    
    * Add socket logic and cursor wrapper
    
    * Remove outdated tests, variables, etc
    
    * height fixed and mouse appears to be tracking properly
    
    * added button functionality for demo
    
    * added button functionality to header nav
    
    * initial outer grid established with major inner components layed out
    
    * setting up initial structure for company profile section
    
    Co-authored-by: Devon Wolfkiel <devonwolf@tutanota.com>
    Co-authored-by: Julianne Vela <75726146+julianne-vela@users.noreply.github.com>

---
+++ src/components/Mission.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './Mission.css';
+
+const Mission = () => {
+  return (
+    <section className={style.mission}>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea rerum harum
+        tenetur. Nihil rerum fugiat culpa repellat aliquid doloribus nisi
+        accusantium et vel facilis obcaecati excepturi ipsum, tenetur libero?
+        Expedita.
+      </p>
+    </section>
+  );
+};
+
+export default Mission;
