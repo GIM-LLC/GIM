dc057601391bea173dd56a49f03d458f3b5c3858
Casey Warren <74938966+caseywar@users.noreply.github.com>
2021-06-10 19:47:01 -0400

    Mission (#12)
    * footer iniital css and ghost icon pulse
    
    * fixed icon pathing on footer
    
    * added mission statement and css

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -4,11 +4,9 @@ import style from './Mission.css';
 const Mission = () => {
   return (
     <section className={style.mission}>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea rerum harum
-        tenetur. Nihil rerum fugiat culpa repellat aliquid doloribus nisi
-        accusantium et vel facilis obcaecati excepturi ipsum, tenetur libero?
-        Expedita.
+      <p className={style.missionHeader}>Core Values</p>
+      <p className={style.missionBody}>
+        Here at General Industrial Manufacturers, we believe in providing the best quality service and products to our loyal customer base. We strive to create economic growth through infrastructure and energy development, and to provide solutions that support communities and protect the planet. Our goal is to provide the highest level of service, the broadest selection of products and the most competitive prices.
       </p>
     </section>
   );
