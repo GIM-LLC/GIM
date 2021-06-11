cbdf6efcb732632cc04a66a7a9ced506e64bd151
Julianne Vela <hello@juliannevela.dev>
2021-06-11 09:32:18 -0700

    Fix/style overhaul (#19)
    * Start basic skeleton of site
    
    * Add socket logic and cursor wrapper
    
    * Remove outdated tests, variables, etc
    
    * height fixed and mouse appears to be tracking properly
    
    * added button functionality for demo
    
    * added button functionality to header nav
    
    * complete style overhaul to refactor grid setup and accommodate relative positioning for cursors
    
    Co-authored-by: Devon Wolfkiel <devonwolf@tutanota.com>
    Co-authored-by: Julianne Vela <75726146+julianne-vela@users.noreply.github.com>

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import React from 'react';
 import style from './HomePage.css';
+
 import CompanyProfile from '../components/profile/CompanyProfile';
 import Mission from '../components/mission/Mission';
 import Gallery from '../components/gallery/Gallery';
