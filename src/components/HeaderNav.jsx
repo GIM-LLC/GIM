d3ba0e5f20b7189faf50133bdc4d2739dc906dcc
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-06-10 23:20:47 -0700

    add sub menus to navbar

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -43,14 +43,37 @@ const HeaderNav = () => {
     socket.on('click', handleButtonChange);
   }, [socket, searchInput]);
 
-
   return (
     <nav className={style.navBar}>
-      <p>link 1</p>
-      <p>link 2</p>
-      <p>link 3</p>
-      <p>link 4</p>
-      <p>link 5</p>
+      <p className={style.navLink}>about</p>
+      <div className={[style.dropList, style.sublist1].join(' ')}>
+        <p className={style.subLink}>our why</p>
+        <p className={style.subLink}>our history</p>
+        <p className={style.subLink}>our team</p>
+      </div>
+      <p className={style.navLink}>locations</p>
+      <div className={[style.dropList, style.sublist2].join(' ')}>
+        <p className={style.subLink}>northeast</p>
+        <p className={style.subLink}>midwest</p>
+        <p className={style.subLink}>coming soon</p>
+      </div>
+      <p className={style.navLink}>press</p>
+      <div className={[style.dropList, style.sublist3].join(' ')}>
+        <p className={style.subLink}>resources</p>
+        <p className={style.subLink}>press sheet</p>
+        <p className={style.subLink}>FAQs</p>
+      </div>
+      <p className={style.navLink}>join us</p>
+      <div className={[style.dropList, style.sublist4].join(' ')}>
+        <p className={style.subLink}>openings</p>
+        <p className={style.specialSubLink}>{`DON'T`}</p>
+        <p className={style.subLink}>benefits</p>
+      </div>
+      <p className={style.navLink}>investors</p>
+      <div className={[style.dropList, style.sublist5].join(' ')}>
+        <p className={style.subLink}>financials</p>
+        <p className={style.subLink}>stock info</p>
+      </div>
       <section>
         <input
           type='text'
