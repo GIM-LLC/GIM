e3dfc97e836ca0a27c4ee50dfda001dbbef89283
Casey Warren <74938966+caseywar@users.noreply.github.com>
2021-06-11 19:57:50 -0400

    Fake stories (#26)
    * added pop up
    
    * added company news stories

--- src/components/profile/CompanyProfile.jsx
+++ src/components/profile/CompanyProfile.jsx
@@ -11,10 +11,7 @@ const CompanyProfile = () => {
         <article>
           <h2>The GIM Story</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-            illo dolores doloremque vitae delectus aut. Officia sunt in labore,
-            rem saepe ullam aut tempore vitae exercitationem ea dolor cupiditate
-            error? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          General Industrial Manufacturing Company is a three-generation, family owned and operated, precision machining supplier. GIM has earned a reputation from our customers as a source supplier of high quality, low cost precision parts with on-time deliveries. We are a top quality team of employees who work well together.
           </p>
         </article>
       </article>
@@ -22,24 +19,21 @@ const CompanyProfile = () => {
       <section className={style.news}>
         <h3>Company News</h3>
         <article>
-          <h6>A Cool Article - 06/04/21</h6>
+          <h6>Manufacturing Leadership Award Winner 2021 - 06/04/21</h6>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum
-            dolorem quasi laborum?
+            This marks the 3rd time GIM has accepted this award since our start in 1962.
           </p>
         </article>
         <article>
-          <h6>Another Cool Article - 06/06/21</h6>
+          <h6>Company Expansion Plan Reveled - 05/22/21</h6>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum
-            dolorem quasi laborum?
+            Plans to expand company reach and hire 100 new employees to start in 2022.
           </p>
         </article>
         <article>
-          <h6>A Boring Article - 06/09/21</h6>
+          <h6>Employee Memorial Fundraiser - 04/01/21</h6>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum
-            dolorem quasi laborum?
+            With the unexpected passing of one of our long time employees, HR has organized a fundraiser to help the family of our late team member.
           </p>
         </article>
       </section>
