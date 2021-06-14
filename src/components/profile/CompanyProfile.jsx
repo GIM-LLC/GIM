1ba4cd424ea866e63d859efb792b328c3cdb8ec4
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-14 16:42:42 -0700

    Add click handler for company story/ghost story, increment points conditionally

--- src/components/profile/CompanyProfile.jsx
+++ src/components/profile/CompanyProfile.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import style from './CompanyProfile.css';
+import { SocketContext } from '../../context/SocketProvider';
+import { GameStateContext } from '../../context/GameStateProvider';
 
 const CompanyProfile = () => {
+  const socket = useContext(SocketContext);
+  const { points, incrementPoints } = useContext(GameStateContext);
+
+  const [showGhostStory, setShowGhostStory] = useState(false);
+  const [ghostStoryCounted, setGhostStoryCounted] = useState(false);
+
+  const handleStoryClick = () => {
+    setShowGhostStory(prev => !prev);
+    
+    if(!ghostStoryCounted) {
+      socket.emit('ghostStoryClick', {
+        ghostStoryCounted: true,
+        points: 1
+      });
+      incrementPoints(1);
+      setGhostStoryCounted(true);
+    }
+  };
+  console.log(points);
   return (
     <section className={style.companyProfile}>
+      
       <article className={style.story}>
         <figure>
           <img src="http://placekitten.com/200/200" alt="gim image" />
         </figure>
-        <article>
-          <h2>The GIM Story</h2>
-          <p>
-          General Industrial Manufacturing Company is a three-generation, family owned and operated, precision machining supplier. GIM has earned a reputation from our customers as a source supplier of high quality, low cost precision parts with on-time deliveries. We are a top quality team of employees who work well together.
-          </p>
-        </article>
+        {showGhostStory
+          ? <article onClick={handleStoryClick}>
+            <h2>The Ghost Story</h2>
+            <p>
+              This is some text about a ghost. There will be some information here. Later. But right now it is just here to fill the space.
+            </p>
+          </article>
+          : <article onClick={handleStoryClick}>
+            <h2>The GIM Story</h2>
+            <p>
+            General Industrial Manufacturing Company is a three-generation, family-owned and operated, precision machining supplier. GIM has earned a reputation as a supplier of high quality, low cost precision parts with on-time deliveries. We are a top quality team of employees who work well together.
+            </p>
+          </article>
+        }
       </article>
 
       <section className={style.news}>
@@ -25,7 +55,7 @@ const CompanyProfile = () => {
           </p>
         </article>
         <article>
-          <h6>Company Expansion Plan Reveled - 05/22/21</h6>
+          <h6>Company Expansion Plan Revealed - 05/22/21</h6>
           <p>
             Plans to expand company reach and hire 100 new employees to start in 2022.
           </p>
