c1c74ad339e7a9e3eff68cdca40368c58cbed208
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-06-15 10:34:54 -0700

    Feature/gallery image buttons (#38)
    * image gallery button challenge
    
    * heroku url

--- src/components/gallery/CaptionedImage.jsx
+++ src/components/gallery/CaptionedImage.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import style from './CaptionedImage.css';
-const CaptionedImage = ({title, text}) => {
+const CaptionedImage = ({
+  title,
+  text,
+  clickHandler,
+  name,
+  currentButtonText,
+  mouseHoverOnHandler,
+  mouseHoverOffHandler,
+  hover,
+  allTrue
+}) => {
   return (
-    <div className={style.container}>
+    <div
+      className={style.container}
+      onMouseEnter={() => mouseHoverOnHandler(name)}
+      onMouseLeave={() => mouseHoverOffHandler(name)}
+    >
       <figure className={style.captionedImage}>
         <img src="https://www.placecage.com/g/200/200" alt="cage" />
       </figure>
-      <div className={style.imageOverlay}>
+      <div
+        className={
+          hover || allTrue
+            ? `${style.hovered} ${style.imageOverlay}`
+            : style.imageOverlay
+        }
+      >
         <div className={style.text}>
           <h3>{title}</h3>
           <div>
             <p>{text}</p>
-            <button>View More</button>
+            <button
+              disabled={allTrue}
+              onClick={() => clickHandler(name)}
+              className={allTrue ? style.pulse : ''}
+            >
+              {currentButtonText}
+            </button>
           </div>
         </div>
       </div>
--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
+import useGalleryImageButtons from '../../hooks/useGalleryImageButtons';
 import CaptionedImage from './CaptionedImage';
 import style from './Gallery.css';
 
 const Gallery = () => {
+  const { imageHoverState, buttonState, allTrue, handleButtonClick, handleMouseOn, handleMouseOff } = useGalleryImageButtons();
   return (
     <section className={style.gallery}>
       <CaptionedImage
+        name="stockholder"
+        hover={imageHoverState.stockholder}
+        clickHandler={handleButtonClick}
+        mouseHoverOnHandler={handleMouseOn}
+        mouseHoverOffHandler={handleMouseOff}
+        currentButtonText={buttonState.stockholder}
+        allTrue={allTrue}
         title="Stockholder Information"
         text="Stockholders Dividens are paid quarterly on the 5th business day of June, August, October, and November at end of day. Stockholders may have their funds deposited via check and direct deposit."
       />
       <CaptionedImage
+        name="employment"
+        hover={imageHoverState.employment}
+        clickHandler={handleButtonClick}
+        mouseHoverOnHandler={handleMouseOn}
+        mouseHoverOffHandler={handleMouseOff}
+        currentButtonText={buttonState.employment}
+        allTrue={allTrue}
         title="Employment"
         text="Supporting you as you make the next step in your career towards state of the art manufacturing technology. Dozens of open roles at your fingertips to help shape the future of industrialized manufacturing. For contracting inquiries please contact via email."
       />
       <CaptionedImage
+        name="life"
+        hover={imageHoverState.life}
+        clickHandler={handleButtonClick}
+        mouseHoverOnHandler={handleMouseOn}
+        mouseHoverOffHandler={handleMouseOff}
+        currentButtonText={buttonState.life}
+        allTrue={allTrue}
         title="Life with GIM"
-        text="Here at GIM you will find minimal bureaucracy, and more support. GIM has a welcoming and warm culture, ready to welcome and serve the community with amazing industrial manufacturing products"
+        text="Here at GIM you will find minimal bureaucracy, and more support. GIM has a welcoming and warm culture, ready to serve the community with amazing industrial manufacturing products"
       />
     </section>
   );
