e96fda86f870536f63a5e701f86e282a55b4cac3
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-06-14 21:50:43 -0700

    answer handling and socket events for search bar input answers

--- src/components/header-footer/SearchForm.jsx
+++ src/components/header-footer/SearchForm.jsx
@@ -1,10 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { SocketContext } from '../../context/SocketProvider';
 import { GameStateContext } from '../../context/GameStateProvider';
+import style from './Header.css';
 
 const SearchForm = () => {
   const socket = useContext(SocketContext);
   const { incrementPoints } = useContext(GameStateContext);
+  const [placeHolderText, setPlaceHolderText] = useState('Search Term');
+  const [buttonText, setButtonText] = useState('SEARCH');
+  const [prompt, setPrompt] = useState(1);
+
+  const disableInputs = (trueORfalse) => {
+    setSearchDisable(trueORfalse);
+    setSearchBtnDisable(trueORfalse);
+  };
 
   //input field
   const [searchDisable, setSearchDisable] = useState(false);
@@ -18,50 +27,84 @@ const SearchForm = () => {
 
   //another user is typing in search box
   const handleSocketInputChange = (newInput) => {
-    setSearchDisable(true);
-    setSearchBtnDisable(true);
+    disableInputs(true);
     setSearchInput(newInput);
   };
 
   //search button do we need to track this at all?
   const [searchBtnDisable, setSearchBtnDisable] = useState(false);
-  const [searchBttnState, setSearchBttnState] = useState(true);
 
   const handleSearchClick = () => {
-    socket.emit('searchSubmit', !searchBttnState);
-    setSearchBttnState((bttnState) => !bttnState);
     setSearchInput('');
-    incrementPoints(1);
-    //what do we do when someone enters a search phrase?
+    // on first answer typed into search box and click submit 
+    if (prompt === 1) {
+      incrementPoints(1);
+      setPrompt(2);
+      setPlaceHolderText('What is my name?');
+      setButtonText('GUESS');
+      socket.emit('searchSubmit', { newPrompt: 2, points: 1, newPlaceholderTxt: 'What is my name?', newButtonTxt: 'GUESS' });
+    } else if (prompt === 2 && searchInput.toUpperCase() === 'ROBIN SMITH') {
+      incrementPoints(2);
+      setPrompt(3);
+      setPlaceHolderText('What is MY core value?');
+      socket.emit('searchSubmit', { newPrompt: 3, points: 2, newPlaceholderTxt: 'What is MY core value?', newButtonTxt: 'GUESS' });
+    } else if (prompt === 3 && searchInput.toUpperCase() === 'ESCAPE') {
+      incrementPoints(2);
+      setPrompt(4);
+      setPlaceHolderText('You\'re getting closer');
+      setButtonText('GO FASTER');
+      disableInputs(true);
+      socket.emit('searchSubmit', { newPrompt: 4, points: 2, newPlaceholderTxt: 'You\'re getting closer', newButtonTxt: 'GO FASTER' });
+    } else if (searchInput.toUpperCase() === 'DUCK') {
+      socket.emit('duck', true);
+    }
   };
 
   //incoming click from socket
-  const handleButtonChange = (newButtonState) => {
-    setSearchDisable(false);
-    setSearchBtnDisable(false);
-    setSearchBttnState(newButtonState);
+  const handleSocketButtonClick = ({ newPrompt, points, newPlaceholderTxt, newButtonTxt }) => {
+    //re-enable input and button
+    disableInputs(false);
+    //update new prompt #
+    setPrompt(newPrompt);
+    //up points 
+    console.log('points coming in from emit: ', points);
+    incrementPoints(points);
+    //set new input placeholder text 
+    setPlaceHolderText(newPlaceholderTxt);
+    //set new button text 
+    setButtonText(newButtonTxt);
+    //clear input field 
     setSearchInput('');
+    //disable search bttn and input if all answers correct 
+    if (newPrompt >= 4) {
+      disableInputs(true);
+    }
   };
 
-  //NEED TO ADD INPUT CHANGE LISTENER TO BACK END
   useEffect(() => {
     socket.on('search input typing', handleSocketInputChange);
-    socket.on('socket serach click', handleButtonChange);
-  }, [socket, searchInput]);
+    socket.on('socket search click', handleSocketButtonClick);
+    return () => {
+      // socket.off('current users', handleCurrentUsers);
+      socket.on('search input typing', handleSocketInputChange);
+      socket.on('socket search click', handleSocketButtonClick);
+    };
+  }, [socket]);
 
   return (
     <section>
       <input
         type="text"
-        placeholder="what do you need?"
+        placeholder={placeHolderText}
         onChange={handleInputChange}
         disabled={searchDisable}
         value={searchInput}
+        className={prompt === 2 || prompt === 3 ? [style.inputBlink, style.pulse].join(' ') : ''}
       />
       <button
         onClick={handleSearchClick}
         disabled={searchBtnDisable}>
-        {searchBttnState ? 'search' : 'CLICKED!'}
+        {buttonText}
       </button>
     </section>
   );
