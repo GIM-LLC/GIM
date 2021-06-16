45dc0718789e39711c434fd035d75c66518d3e54
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-15 17:15:49 -0700

    Commit to pull

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,14 +7,16 @@ import Popup from '../components/popup/Popup';
 import { useState, useEffect } from 'react';
 
 export default function App() {
+  
   const [timedPopup, setTimedPopup] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {setTimedPopup(true);}, 9000);
- 
   }, []);
+
+
+
   return (
-  
     <SocketContext.Provider value={socket}>
       <GameStateProvider>
         <Popup trigger={timedPopup} setTrigger={setTimedPopup} />
