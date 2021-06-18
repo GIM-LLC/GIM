637b6daa0f63ac6239915739aa4a19e94ef61f70
Casey Warren <74938966+caseywar@users.noreply.github.com>
2021-06-17 21:04:58 -0400

    Update SocketProvider.jsx

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -1,14 +1,15 @@
 import { createContext } from 'react';
 import io from 'socket.io-client';
 
-// this URL is the sandbox
-// const sandbox = 'https://gim-llc.herokuapp.com';
 
 // whatever localhost strikes your fancy
 // const url = 'http://localhost:8080';
 
-// this is the real backend
-const url = 'https://gim-server.herokuapp.com';
+// this is the staging backend
+// const url = 'https://gim-server.herokuapp.com';
+
+// this is the live main backend
+const url = 'https://gim-llc-main.herokuapp.com/';
 
 export const socket = io(url, {
   cors: true,
