4c5f7e2e2d22cb0a737d28ba5091c94b7601bcbb
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-06-17 19:46:21 -0700

    main heroku url

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -3,13 +3,13 @@ import io from 'socket.io-client';
 
 
 // whatever localhost strikes your fancy
-const url = 'http://localhost:8080';
+// const url = 'http://localhost:8080';
 
 // this is the staging backend
 // const url = 'https://gim-server.herokuapp.com';
 
 // this is the live main backend
-// const url = 'https://gim-llc-main.herokuapp.com/';
+const url = 'https://gim-llc-main.herokuapp.com/';
 
 export const socket = io(url, {
   cors: true,
